8d47a0fcdb2c8e84fd1444cf1693e14fec6297b0
Hank Anderson <hank@statease.com>
2016-01-17 15:31:38 -0600

    match_libs no longer applies -s when the is_static flag is set.
    The -s in boost naming convention is meant to indicate that the
    runtime is linked statically, not that the library itself is static.
    This was causing link errors when statically linking boost libraries
    that link dynamically to the runtime.

--- waflib/extras/boost.py
+++ waflib/extras/boost.py
@@ -268,8 +268,6 @@ def boost_get_libs(self, *k, **kw):
 			t.append('-mt')
 		if kw.get('abi', None):
 			t.append('%s%s' % (is_static and '-s' or '-', kw['abi']))
-		elif is_static:
-			t.append('-s')
 		tags_pat = t and ''.join(t) or ''
 		ext = is_static and self.env.cxxstlib_PATTERN or self.env.cxxshlib_PATTERN
 		ext = ext.partition('%s')[2] # remove '%s' or 'lib%s' from PATTERN
